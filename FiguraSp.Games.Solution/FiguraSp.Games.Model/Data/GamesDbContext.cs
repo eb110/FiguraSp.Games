7512b8fb2e58f4a599685398f31f594ff967b5d5
Wladyslaw Figura <wfigura@op.pl>
2026-04-19 21:03:52 +0100

    14a_games scaffold

--- FiguraSp.Games.Solution/FiguraSp.Games.Model/Data/GamesDbContext.cs
+++ FiguraSp.Games.Solution/FiguraSp.Games.Model/Data/GamesDbContext.cs
@@ -25,5 +25,55 @@ namespace FiguraSp.Games.Model.Data
 
             return entities;
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("Polish_100_CS_AS_KS_WS_SC_UTF8");
+
+            modelBuilder.Entity<Event>(entity =>
+            {
+                entity.HasIndex(e => e.GameId, "IX_Events_GameId");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                entity.Property(e => e.HomeAway).HasDefaultValue("");
+
+                entity.HasOne(d => d.Game).WithMany(p => p.Events).HasForeignKey(d => d.GameId);
+            });
+
+            modelBuilder.Entity<PicklistGameLevel>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Picklist__3214EC0708777185");
+
+                entity.ToTable("PicklistGameLevel");
+
+                entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+                entity.Property(e => e.GameLevel).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Season>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<VRiderEvent>(entity =>
+            {
+                entity
+                    .HasNoKey()
+                    .ToView("V_Rider_Events");
+
+                entity.Property(e => e.Heats).HasMaxLength(4000);
+                entity.Property(e => e.Name)
+                    .HasMaxLength(30)
+                    .UseCollation("Persian_100_CS_AS_KS_WS_SC_UTF8");
+                entity.Property(e => e.Rows).HasMaxLength(4000);
+                entity.Property(e => e.Surname)
+                    .HasMaxLength(30)
+                    .UseCollation("Persian_100_CS_AS_KS_WS_SC_UTF8");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
